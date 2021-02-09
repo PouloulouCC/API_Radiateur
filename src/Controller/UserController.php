b791227bbaf30509d02bfad26020029b25397bff
louisasco <asc0lou1s>
2021-02-09 22:38:03 +0100

    jwt config

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +18,19 @@ class UserController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
         ]);
+    }
+
+    /**
+     * @Route("/login_check", name="login")
+     * @return JsonResponse
+     */
+    public function login(): JsonResponse
+    {
+        $user = $this->getUser();
+
+        return $this->json(array(
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ));
     }
 }
