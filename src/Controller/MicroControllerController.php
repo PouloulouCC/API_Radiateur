ef2d8428993764903406dd4bc98a25d7871379b9
louisasco <asc0lou1s>
2021-02-21 10:22:29 +0100

    modifs

--- src/Controller/MicroControllerController.php
+++ src/Controller/MicroControllerController.php
@@ -166,84 +166,84 @@ class MicroControllerController extends AbstractController
         );
     }
 
-    /**
-     * @Route("/getData", name="get_data")
-     */
-    public function getDataAction(Request $request): Response
-    {
-        $data = json_decode($request->getContent());
-        $em = $this->getDoctrine()->getManager();
+//    /**
+//     * @Route("/getData", name="get_data")
+//     */
+//    public function getDataAction(Request $request): Response
+//    {
+//        $data = json_decode($request->getContent());
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $microController = new MicroController();
+//        $microController = $em->getRepository("App:MicroController")->findOneBy(['macAddress' => $data->mac]);
+//
+//
+//
+//        if($microController != null) {
+//            return new JsonResponse(
+//                array(
+//                    "temp" => $microController->getCurrentTemperature(),
+//                    "tempMax" => $microController->getTempMax(),
+//                    "tempMin" => $microController->getTempMin(),
+//                    "hours" => $microController->getHours(),
+//                )
+//            );
+//        }
+//        $response = new Response();
+//        $response->setStatusCode(404);
+//        return $response;
+//    }
 
-        $microController = new MicroController();
-        $microController = $em->getRepository("App:MicroController")->findOneBy(['macAddress' => $data->mac]);
+//    /**
+//     * @Route("/getConfig", name="get_config")
+//     */
+//    public function getConfigAction(Request $request): Response
+//    {
+//        $data = json_decode($request->getContent());
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $microController = new MicroController();
+//        $microController = $em->getRepository("App:MicroController")->findOneBy(['macAddress' => $data->mac]);
+//
+//        if($microController != null) {
+//            return new JsonResponse(
+//                array(
+//                    "mode" => $microController->getMode(),
+//                    "tempMax" => $microController->getTempMax(),
+//                    "tempMin" => $microController->getTempMin(),
+//                    "hours" => $microController->getHours(),
+//                )
+//            );
+//        }
+//        $response = new Response();
+//        $response->setStatusCode(404);
+//        return $response;
+//    }
 
-
-
-        if($microController != null) {
-            return new JsonResponse(
-                array(
-                    "temp" => $microController->getCurrentTemperature(),
-                    "tempMax" => $microController->getTempMax(),
-                    "tempMin" => $microController->getTempMin(),
-                    "hours" => $microController->getHours(),
-                )
-            );
-        }
-        $response = new Response();
-        $response->setStatusCode(404);
-        return $response;
-    }
-
-    /**
-     * @Route("/getConfig", name="get_config")
-     */
-    public function getConfigAction(Request $request): Response
-    {
-        $data = json_decode($request->getContent());
-        $em = $this->getDoctrine()->getManager();
-
-        $microController = new MicroController();
-        $microController = $em->getRepository("App:MicroController")->findOneBy(['macAddress' => $data->mac]);
-
-        if($microController != null) {
-            return new JsonResponse(
-                array(
-                    "mode" => $microController->getMode(),
-                    "tempMax" => $microController->getTempMax(),
-                    "tempMin" => $microController->getTempMin(),
-                    "hours" => $microController->getHours(),
-                )
-            );
-        }
-        $response = new Response();
-        $response->setStatusCode(404);
-        return $response;
-    }
-
-    /**
-     * @Route("/updateConfig", name="update_config")
-     */
-    public function updateConfigAction(Request $request): Response
-    {
-        $data = json_decode($request->getContent());
-        $em = $this->getDoctrine()->getManager();
-
-        $microController = new MicroController();
-        $microController = $em->getRepository("App:MicroController")->findOneBy(['macAddress' => $data->mac]);
-
-        if ($microController != null) {
-            $microController->setMacAddress($data->mac);
-            $microController->setMode($data->mode);
-            $microController->setTempMax($data->tempMax);
-            $microController->setTempMin($data->tempMin);
-            $microController->setHours($data->hours);
-
-            $response = new Response();
-            $response->setStatusCode(204);
-            return $response;
-        }
-        $response = new Response();
-        $response->setStatusCode(404);
-        return $response;
-    }
+//    /**
+//     * @Route("/updateConfig", name="update_config")
+//     */
+//    public function updateConfigAction(Request $request): Response
+//    {
+//        $data = json_decode($request->getContent());
+//        $em = $this->getDoctrine()->getManager();
+//
+//        $microController = new MicroController();
+//        $microController = $em->getRepository("App:MicroController")->findOneBy(['macAddress' => $data->mac]);
+//
+//        if ($microController != null) {
+//            $microController->setMacAddress($data->mac);
+//            $microController->setMode($data->mode);
+//            $microController->setTempMax($data->tempMax);
+//            $microController->setTempMin($data->tempMin);
+//            $microController->setHours($data->hours);
+//
+//            $response = new Response();
+//            $response->setStatusCode(204);
+//            return $response;
+//        }
+//        $response = new Response();
+//        $response->setStatusCode(404);
+//        return $response;
+//    }
 }
