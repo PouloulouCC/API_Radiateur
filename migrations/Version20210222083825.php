c5c5c721f1772f93dea851e1ce5756b8a984fe13
PouloulouCC <louis.coudurier-curveur@etu.univ-lyon1.fr>
2021-02-22 09:38:46 +0100

    migration

---
+++ migrations/Version20210222083825.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210222083825 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE micro_controller ADD active TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE period ADD active TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE micro_controller DROP active');
+        $this->addSql('ALTER TABLE period DROP active');
+    }
+}
